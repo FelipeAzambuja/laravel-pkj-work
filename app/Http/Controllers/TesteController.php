a34ec8880026874c55451cf77f98acefa1b75aae
Felipe Nunes Azambuja <felipe@newbgp.com.br>
2019-12-11 13:13:34 -0300

    up

---
+++ app/Http/Controllers/TesteController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TesteController extends Controller {
+
+    // rota /teste
+    function index () {
+        
+    }
+
+    // rota /teste/teste
+    function teste ( Request $form ) {
+        
+    }
+
+}
